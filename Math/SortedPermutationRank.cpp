3d48034c90c4098ccb253036977213f52ab776af
aviral243 <aviralgangwar24@gmail.com>
2020-04-14 00:00:59 +0530

    math done

--- Math/SortedPermutationRank.cpp
+++ Math/SortedPermutationRank.cpp
@@ -6,24 +6,22 @@ int fact(int n)
 }
 
 int Solution::findRank(string A) {
-    
-    
 
-                    string s=A ;
-                    //cin>>s;
-                    int ans =0;
-                    int n=s.length();
-                    for(int i=0;i<n-1;i++)
-                        {
-                            int c=0;
-                            for(int j=i+1;j<n;j++)
-                                {
-                                        if(s[j]<s[i])
-                                            c++;
-                                }   
-                                ans+=( (c*fact(n-i-1)) )%mod;
-                                //cout << c <<" "<<ans<< endl;
-                        }
-                        return (ans+1 )%mod;
-                        //cout << ans+1 ;
+    string s=A ;
+    //cin>>s;
+    int ans =0;
+    int n=s.length();
+    for(int i=0;i<n-1;i++)
+        {
+            int c=0;
+            for(int j=i+1;j<n;j++)
+                {
+                        if(s[j]<s[i])
+                            c++;
+                }   
+                ans+=( (c*fact(n-i-1)) )%mod;
+                //cout << c <<" "<<ans<< endl;
+        }
+        return (ans+1 )%mod;
+        //cout << ans+1 ;
 }
